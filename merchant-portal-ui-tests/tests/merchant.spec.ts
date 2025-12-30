34927db7fd729584ad6a6767c93c096f5e73b5de
Raymond Sekikubo <raymondskkb@gmail.com>
2025-12-31 00:50:58 +0300

    merchant-spec - changes

--- merchant-portal-ui-tests/tests/merchant.spec.ts
+++ merchant-portal-ui-tests/tests/merchant.spec.ts
@@ -4,3 +4,15 @@ test('opens homepage', async ({ page }) => {
   await page.screenshot({ path: 'google.png' });
   await expect(page).toHaveTitle(/Google/);
 });
+
+test('opens homepage 1', async ({ page }) => {
+  await page.goto('https://example.com/');
+  await page.screenshot({ path: 'example.png' });
+  await expect(page).toHaveTitle(/Example/);
+});
+
+test('opens homepage 2', async ({ page }) => {
+  await page.goto('https://en.wikipedia.org/wiki/1');
+  await page.screenshot({ path: 'wikipedia.png' });
+  await expect(page).toHaveTitle(/wikipedia/);
+});
