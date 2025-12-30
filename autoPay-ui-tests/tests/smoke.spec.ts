a7d0db8ff3c77897447eb9b43cd707d093a6c0bf
Raymond Sekikubo <raymondskkb@gmail.com>
2025-12-31 00:20:28 +0300

    fix - autopay test

--- autoPay-ui-tests/tests/smoke.spec.ts
+++ autoPay-ui-tests/tests/smoke.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 test('opens homepage', async ({ page }) => {
   await page.goto('https://example.com');
+  await page.screenshot({ path: 'homepage.png' });
   await expect(page).toHaveTitle(/Example/);
 });
